44e3685d3ba85e66fffd46f3cf7faf75d968f0e6
HeliumSquid <HeliumSquid@users.noreply.github.com>
2015-07-21 17:58:43 +0200

    Updated gh-pages.
    One step closer to v.1.0.0.

---
+++ js/demo.js
@@ -0,0 +1,8 @@
+$(function() {
+  $('.scroll').click(function(e) {
+    e.preventDefault();
+    $('body, html').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 750);
+  });
+});
